4489f3261ad591de7947c5e809987d1b6ad7f251
gui <guigon@alunos.utfpr.edu.br>
2023-09-02 11:00:04 -0300

    feat: add Atividade2 Mensagens

--- script.js
+++ script.js
@@ -9,4 +9,14 @@ const atividade1 = (num) => {
   return fatorial;
 };
 
+const atividade2 = (n, mensagem) => {
+  stringRetorno = mensagem;
+  for (i = 1; i < n; i++) {
+    stringRetorno += " ";
+    stringRetorno += mensagem;
+  }
+  return stringRetorno;
+};
+
 console.log(atividade1(6));
+console.log(atividade2(5, "hello-world"));
